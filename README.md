f10390a42af7f5fd837ce0e3a6f518ed7013f1a9
Stephan Jarisch <75485556+StephanJarisch@users.noreply.github.com>
2023-10-11 18:17:54 +0200

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# yt-image-to-text
+Certainly! Here's an updated version of the README file:
+
+# Image to Text Converter
+
+This repository contains a Python script that extracts text from images using Optical Character Recognition (OCR) technology. It utilizes the Tesseract OCR engine and OpenCV library to process images and convert them into text.
+
+## Getting Started
+
+To get started with this project, follow the steps below:
+
+1. Clone the repository to your local machine:
+
+   ```
+   git clone https://github.com/AILETIC/yt-image-to-text.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```
+   cd yt-image-to-text
+   ```
+
+3. Create a virtual environment:
+
+   - For macOS/Linux:
+
+     ```
+     python3 -m venv env
+     ```
+
+   - For Windows:
+
+     ```
+     python -m venv env
+     ```
+
+4. Activate the virtual environment:
+
+   - For macOS/Linux:
+
+     ```
+     source env/bin/activate
+     ```
+
+   - For Windows:
+
+     ```
+     .\env\Scripts\activate
+     ```
+
+5. Upgrade pip:
+
+   - For macOS/Linux:
+
+     ```
+     pip3 install --upgrade pip
+     ```
+
+   - For Windows:
+
+     ```
+     pip install --upgrade pip
+     ```
+
+6. Install the required dependencies:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+7. Run the main script:
+
+   - For macOS/Linux:
+
+     ```
+     python3 main.py
+     ```
+
+   - For Windows:
+
+     ```
+     python main.py
+     ```
+
+## Usage
+
+The main script (`main.py`) performs the following steps:
+
+1. Retrieves a list of image file paths from a specified directory.
+2. Converts the images into RGB format.
+3. Extracts text from the images using Tesseract OCR.
+4. Saves the extracted text as a text file and as a PDF.
+
+You can modify the script to suit your specific requirements, such as changing the input directory or language settings.
+
+## Contributing
+
+Contributions to this project are welcome. If you encounter any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+Feel free to customize this README file according to your needs. Let me know if there's anything else I can help you with!
---
+++ main.py
@@ -0,0 +1,188 @@
+import tqdm 
+import os
+import pytesseract
+import json
+import img2pdf
+import cv2
+
+def get_pages(
+        DIR:str, 
+        file_type:str = "JPG"
+    ) -> list:
+
+    """ returns a list of file paths from a certain directory """
+
+    pages = []
+    for path in os.listdir(DIR):
+        if path[-len(file_type):] == file_type:
+            pages.append(DIR + path)
+
+    pages = sorted(pages)
+    return pages
+
+def get_pages(
+        DIR:str, 
+        file_type:str = "JPG"
+    ) -> list:
+
+    """ returns a list of file paths from a certain directory """
+
+    return sorted([
+        DIR + elem 
+        for elem 
+        in os.listdir(DIR) 
+        if elem[-len(file_type):] == file_type
+    ])
+
+def get_images(
+        pages:list
+    ) -> list:
+    """ returns a list of ndarray (images, rgb) """
+    images = []
+    for path in pages:
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        images.append(img)
+    return images
+
+def get_images(pages:list) -> list:
+
+    """ returns a list of ndarray (images, rgb) """
+
+    return [
+        cv2.cvtColor(
+            cv2.imread(elem), 
+            cv2.COLOR_BGR2RGB
+        ) 
+        for elem in pages
+    ]
+
+def get_text_from_images(
+        images, 
+        language="eng", 
+        config="--psm 6"
+    ) -> dict:
+
+    """ 
+        returns a dictonary, containing a list of pages 
+        with there respective text 
+
+        Page segmentation modes:
+        0    Orientation and script detection (OSD) only.
+        1    Automatic page segmentation with OSD.
+        2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+        3    Fully automatic page segmentation, but no OSD. (Default)
+        4    Assume a single column of text of variable sizes.
+        5    Assume a single uniform block of vertically aligned text.
+        6    Assume a single uniform block of text.
+        7    Treat the image as a single text line.
+        8    Treat the image as a single word.
+        9    Treat the image as a single word in a circle.
+        10    Treat the image as a single character.
+        11    Sparse text. Find as much text as possible in no particular order.
+        12    Sparse text with OSD.
+        13    Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
+    
+    """
+
+    data = {
+        "pages": []
+    }
+
+    for image in tqdm.tqdm(images):
+        text = pytesseract.image_to_string(
+            image, 
+            lang=language, 
+            config=config
+        )
+        data["pages"].append(text)
+
+    return data
+
+def get_text_from_images(
+        images,
+        language="eng",
+        config="--psm 6"
+    ) -> dict:
+
+    """ 
+        returns a dictonary, containing a list of pages with there respective text
+
+        Page segmentation modes:
+        0    Orientation and script detection (OSD) only.
+        1    Automatic page segmentation with OSD.
+        2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+        3    Fully automatic page segmentation, but no OSD. (Default)
+        4    Assume a single column of text of variable sizes.
+        5    Assume a single uniform block of vertically aligned text.
+        6    Assume a single uniform block of text.
+        7    Treat the image as a single text line.
+        8    Treat the image as a single word.
+        9    Treat the image as a single word in a circle.
+        10    Treat the image as a single character.
+        11    Sparse text. Find as much text as possible in no particular order.
+        12    Sparse text with OSD.
+        13    Raw line. Treat the image as a single text line, bypassing hacks that are Tesseract-specific.
+        
+    """
+    return {
+        "pages": [
+            pytesseract.image_to_string(
+                image, 
+                lang=language, 
+                config=config
+            ) 
+            for image 
+            in tqdm.tqdm(images)
+        ]
+    }
+
+def save_text(
+        data:dict,
+        filename:str = "output",
+        type:str ="txt"
+    ) -> None:
+
+    """ saves the extracted text either as a whole as text file or as json as dictonary """
+
+    if type == "txt":
+        with open(filename + ".txt", "w") as fp:
+            fp.write("".join(data["pages"]))
+    
+    elif type == "json":
+        with open(filename + ".json", "w") as fp:
+            json.dump(data, fp)
+    
+    else:
+        print("warning: not the correct file type, choose between 'txt' and 'json'")
+        
+def save_pdf(
+        images:list,
+        filename:str = "output"
+    ) -> None:
+
+    """ saves images as pdf """
+
+    pdf_data = img2pdf.convert(images)
+
+    with open(filename + ".pdf", "wb") as file:
+        file.write(pdf_data)
+
+def main():
+    DIR = "pages/"
+
+    # get img file paths
+    pages = get_pages(DIR)
+
+    # get book pages as images
+    images = get_images(pages)
+    
+    # get text and data from images (data -> dict)
+    data = get_text_from_images(images, language="eng")
+
+    # saves as text and as pdf
+    save_text(data, type="txt")
+    save_pdf(images)
+
+if __name__ == "__main__":
+    main()
